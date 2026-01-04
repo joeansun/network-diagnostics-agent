e340a499b00f64a89f9a8f1eef7f654f7d1810e7
Joean Sun <joean.aus@gmail.com>
2026-01-04 15:04:41 +0800

    small modified for parse ping function

--- src/netdiag/analysis/ping.py
+++ src/netdiag/analysis/ping.py
@@ -3,20 +3,31 @@ import subprocess
 from itertools import dropwhile
 
 
-def parse_ping(raw_input: str) -> str:
+def parse_ping(raw_input: str) -> dict:
+    ping_info = dict()
     splitted_inputs = raw_input.splitlines()
 
     # extract the statistics for ping 
     result = dropwhile(lambda line: "statistics" not in line, splitted_inputs)
 
     # extract the address or hostname from the statistics title
-    address = re.search(r"^---\s([A-Za-z0-9][A-Za-z0-9.-]*)\s", next(result)).group(1)
+    m = re.search(r"^---\s([A-Za-z0-9][A-Za-z0-9.-]*)\s", next(result))
+    if not m:
+        raise ValueError("Could not parse address")
+    ping_info["address"] = m.group(1)
 
     # extract the info about packet transmission
     packets_info = next(result).split(", ")
+    ping_info["packets_info"] = packets_info
 
     # min / avg / max / stddev 
-    stats_data = re.search(r"=\s*([\d.]+)/([\d.]+)/([\d.]+)/([\d.]+)", next(result)).groups()
+    m = re.search(r"=\s*([\d.]+)/([\d.]+)/([\d.]+)/([\d.]+)", next(result))
+    if not m:
+        raise ValueError("Could not parse stats")
+    ping_info["stats_data"] = m.groups()
+
+    
+    return ping_info 
 
 def ping_anaylsis(raw_input: str) -> None:
     return
