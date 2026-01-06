979d5604c5cd9643dc0130db07fa136409c80c81
Joean Sun <joean.aus@gmail.com>
2026-01-06 17:52:38 +0800

    partially done ping_metrics, ping_singals in analysis.ping and data.ping

--- src/netdiag/analysis/ping.py
+++ src/netdiag/analysis/ping.py
@@ -1,6 +1,12 @@
 import re
 import subprocess
+from enum import Enum
+from netdiag.data.ping import *
 from itertools import dropwhile
+
+class DiagnosisCause(str, Enum):
+    OK = "ok"
+
 
 
 def parse_ping(raw_input: str) -> dict:
@@ -18,13 +24,15 @@ def parse_ping(raw_input: str) -> dict:
 
     # extract the info about packet transmission
     packets_info = next(result).split(", ")
-    ping_info["packets_info"] = packets_info
+    packets_number = [re.search(r"^[\d.]+", s).group(0) for s in packets_info]
+    ping_info["packets_info"] = packets_number
 
     # min / avg / max / stddev 
     m = re.search(r"=\s*([\d.]+)/([\d.]+)/([\d.]+)/([\d.]+)", next(result))
     if not m:
         raise ValueError("Could not parse stats")
-    ping_info["stats_data"] = m.groups()
+    stats_data_group = [float(s) for s in m.groups()]
+    ping_info["stats_data"] = stats_data_group
 
     
     return ping_info 
@@ -32,6 +40,30 @@ def parse_ping(raw_input: str) -> dict:
 def ping_anaylsis(raw_input: str) -> None:
     ping_info = parse_ping(raw_input)
     print(ping_info)
+
+    ping_metrics = PingMetrics(
+        sent=int(ping_info["packets_info"][0]),
+        received=int(ping_info["packets_info"][1]),
+        loss_pct_perc=float(ping_info["packets_info"][2]),
+        rtt_min_ms=ping_info["stats_data"][0],
+        rtt_avg_ms=ping_info["stats_data"][1],
+        rtt_max_ms=ping_info["stats_data"][2],
+        rtt_stddev_ms=ping_info["stats_data"][3]
+    )
+
+    ping_signals = PingSignals(
+        high_loss=ping_metrics.loss_pct_perc >= HIGH_PACKET_LOSS_THRESHOLD_PCT,
+        high_latency=ping_metrics.rtt_avg_ms >= HIGH_LATENCY_THRESHOLD_MS,
+        unstable=(
+            ping_metrics.rtt_stddev_ms >= UNSTABLE_DEVIATION * ping_metrics.rtt_avg_ms
+            or (ping_metrics.rtt_max_ms - ping_metrics.rtt_min_ms) >= 100.0
+        )
+        
+    )
+
+    print(ping_metrics)
+    print(ping_signals)
+    
 
 
 command = "ping -c 5 google.com"
