2122ee33bd986623ca64b5c57c48876df2b090d3
Joean Sun <joean.aus@gmail.com>
2026-01-08 11:18:02 +0800

    refactor(analysis.ping.py): refactored build metrics signals diagnosis and introduec summarise causes function

--- src/netdiag/analysis/ping.py
+++ src/netdiag/analysis/ping.py
@@ -1,4 +1,5 @@
 import re
+import math
 import subprocess
 from enum import Enum
 from netdiag.data.ping import *
@@ -44,60 +45,95 @@ def parse_ping(raw_input: str) -> dict:
     stats_data_group = [float(s) for s in m.groups()]
     ping_info["stats_data"] = stats_data_group
 
-    # calcualte jitter
-    abs_std = [abs(value - ping_info["stats_data"][1]) for value in time_each_run]
-    print(ping_info["stats_data"][1])
-    print(abs_std)
-    print(sum(abs_std))
-
+    # calcualte jitter, jitter_ratio
+    if len(time_each_run) < 2:
+        ping_info["jitter"] = 0.0
+        ping_info["jitter_ratio"] = 0.0
+    else:
+        average_delay = ping_info["stats_data"][1]
+        sum_of_squared_difference = sum([(value - average_delay) ** 2 for value in time_each_run])
+        jitter = math.sqrt(sum_of_squared_difference / len(time_each_run))
+        ping_info["jitter"] = jitter
+        ping_info["jitter_ratio"] =  jitter / average_delay
     
     return ping_info 
 
-def ping_anaylsis(raw_input: str) -> None:
+# DiagnosisCause located in data/ping.py
+def diagnose_from_signals(signal: PingSignals) -> DiagnosisCause:
+    if signal.no_reply: return DiagnosisCause.NO_CONNECTIVITY
+    if signal.high_loss: return DiagnosisCause.HIGH_LOSS
+    if signal.unstable_jitter: return DiagnosisCause.UNSTABLE_JITTER
+    if signal.high_latency: return DiagnosisCause.HIGH_LATENCY
+    return DiagnosisCause.OK
+
+def build_ping_metrics(ping_info: dict) -> PingMetrics:
+    sent, received, loss_pct_perc = ping_info["packets_info"]
+    rtt_min, rtt_avg, rtt_max, rtt_stddev = ping_info["stats_data"]
+    return PingMetrics(
+        sent = int(sent),
+        received = int(received),
+        loss_pct_perc = float(loss_pct_perc),
+        rtt_min_ms = rtt_min,
+        rtt_avg_ms = rtt_avg,
+        rtt_max_ms = rtt_max,
+        rtt_stddev_ms = rtt_stddev,
+        jitter = ping_info["jitter"],
+        jitter_ratio = ping_info["jitter_ratio"]
+    )
+
+def build_ping_signals(ping_metrics: PingMetrics) -> PingSignals:
+    return PingSignals(
+        no_reply = ping_metrics.received == 0,
+        any_loss = ping_metrics.loss_pct_perc > 0.0,
+        high_loss = ping_metrics.loss_pct_perc >= HIGH_PACKET_LOSS_THRESHOLD_PCT,
+        high_latency = ping_metrics.rtt_avg_ms >= HIGH_LATENCY_THRESHOLD_MS,
+        unstable_jitter = ping_metrics.jitter_ratio > UNSTABLE_JITTER,
+        unstable = (
+            ping_metrics.rtt_stddev_ms >= UNSTABLE_DEVIATION * ping_metrics.rtt_avg_ms
+            or (ping_metrics.rtt_max_ms - ping_metrics.rtt_min_ms) >= 100.0
+        )
+    )
+
+# CAUSE_SUMMARY located in data/ping.py 
+def summarise_causes(causes: DiagnosisCause) -> str:
+    return CAUSE_SUMMARY.get(causes, "Unknown or unclassified condition.")
+
+def compute_confidence(ping_metrics: PingMetrics, ping_signals: PingSignals, causes: DiagnosisCause) -> float:
+    ...
+
+def summarise_evidence() -> dict[str, str]:
+    ...
+
+def build_ping_diagnosis(ping_signals: PingSignals) -> PingDiagnosis:
+    causes = diagnose_from_signals(ping_signals)
+    return PingDiagnosis(
+        causes = causes,
+        summary = summarise_causes(causes),
+        confidence = compute_confidence(),
+        evidence = summarise_evidence()
+    )
+
+def build_record(ping_info: dict, ping_metrics: PingMetrics, ping_signals: PingSignals, ping_diagnosis: PingDiagnosis) -> PingRecord:
+    return PingRecord(
+        run_id = datetime.now(timezone.utc).strftime('%Y%m%dT%H%M%S%fZ'),
+        timestamp = datetime.now(timezone.utc).strftime("%Y-%m-%dT%H:%M:%SZ"),
+        target = ping_info["address"],
+        metrics = ping_metrics,
+        signals = ping_signals,
+        diagnosis = ping_diagnosis
+    )
+
+def ping_analysis(raw_input: str) -> None:
     ping_info = parse_ping(raw_input)
     print(ping_info)
 
-    # ping_metrics = PingMetrics(
-    #     sent=int(ping_info["packets_info"][0]),
-    #     received=int(ping_info["packets_info"][1]),
-    #     loss_pct_perc=float(ping_info["packets_info"][2]),
-    #     rtt_min_ms=ping_info["stats_data"][0],
-    #     rtt_avg_ms=ping_info["stats_data"][1],
-    #     rtt_max_ms=ping_info["stats_data"][2],
-    #     rtt_stddev_ms=ping_info["stats_data"][3]
-    # )
+    ping_metrics = build_ping_metrics(ping_info)
+    ping_signals = build_ping_signals(ping_metrics)
+    ping_diagnosis = build_ping_diagnosis(ping_signals)
+    ping_record = build_record(ping_info, ping_metrics, ping_signals, ping_diagnosis)
 
-    # ping_signals = PingSignals(
-    #     no_reply=ping_metrics.received == 0,
-    #     any_loss=ping_metrics.sent - ping_metrics.received > 0,
-    #     high_loss=ping_metrics.loss_pct_perc >= HIGH_PACKET_LOSS_THRESHOLD_PCT,
-    #     high_latency=ping_metrics.rtt_avg_ms >= HIGH_LATENCY_THRESHOLD_MS,
-    #     unstable=(
-    #         ping_metrics.rtt_stddev_ms >= UNSTABLE_DEVIATION * ping_metrics.rtt_avg_ms
-    #         or (ping_metrics.rtt_max_ms - ping_metrics.rtt_min_ms) >= 100.0
-    #     )
-    # )
-
-    # ping_diagnosis = PingDiagnosis(
-    #     causes : str
-    #     summary : str
-    #     confidence : float
-    #     evidence : dict[str, float]
-    # )
-
-    # ping_record = PingRecord(
-    #     run_id=datetime.now(timezone.utc).strftime('%Y%m%dT%H%M%S%fZ'),
-    #     timestamp=datetime.now(timezone.utc).strftime("%Y-%m-%dT%H:%M:%SZ"),
-    #     target=ping_info["address"],
-    #     metrics=ping_metrics,
-    #     signals=ping_signals,
-    #     diagnosis=ping_diagnosis
-    # )
-
-
-
-    # print(ping_metrics)
-    # print(ping_signals)
+    print(ping_metrics)
+    print(ping_signals)
     
 
 
@@ -111,4 +147,4 @@ result = subprocess.run(
         )
 print(result.stdout)
 print()
-ping_anaylsis(result.stdout)
+ping_analysis(result.stdout)
